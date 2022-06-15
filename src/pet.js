2989bc64e70e15b4bb1ba95bcd2c01ae0eea909f
SuzBarnes <SuzBarnes@github.com>
2022-06-15 16:35:09 +0100

    walk test

--- src/pet.js
+++ src/pet.js
@@ -1,8 +1,9 @@
 const MAXIMUM_FITNESS = 10;
+const STARTATZERO = 0;
 function Pet(name) {
     this.name = name;
-    this.age =0,
-    this.hunger = 0;
+    this.age = STARTATZERO,
+    this.hunger = STARTATZERO;
     this.fitness =10;
     this.growUp = function () {
         this.age+=1;

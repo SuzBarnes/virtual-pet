633d5f1056b1343a985c7bfe5ab79824237e5b49
SuzBarnes <SuzBarnes@github.com>
2022-06-15 16:30:40 +0100

    walk test

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -16,9 +16,49 @@ describe('constructor', () => {
   describe('growUp'), () => {
     it ('increments the age by 1', () => {
       const pet = new Pet('Fido');
-
       pet.growUp();
-
       expect (pet.age).toEqual(1);
     });
   }
+  describe ('hunger'), () => {
+    it('has an initial hunger of 0', () => {
+      const pet = new Pet ('Fido');
+      expect (pet.hunger).toEqual (0);
+    });
+    it ('increases by 5 when growUp occurs', () => {
+      const pet = new Pet ('Fido');
+      pet.growUp();
+      expect (pet.hunger).toEqual(5);
+    });
+  }
+
+  describe ('fitness'), () => {
+    it('has an initial fitness of 10', () => {
+      const pet = new Pet ('Fido');
+      expect (pet.fitness).toEqual(10);
+    })
+    it ('decreases by 3 when growUp occurs', () => {
+const pet = new Pet ('Fido');
+pet.growUp();
+expect (pet.fitness).toEqual(7);
+    });
+  }
+  
+  describe ('walk'), () => {
+    it ('increases fitness by 4', ()=> {
+      const pet = new Pet ('Fido');
+
+      pet.fitness = 4;
+      pet.walk();
+
+    expect (pet.fitness()).toEqual(8);
+  })
+  it('increases fitness by a maximum of 10', () => {
+    const pet = new Pet('Fido');
+
+    pet.fitness=8;
+    pet.walk();
+
+    expect(pet.fitness).toEqual(10);
+  });
+};

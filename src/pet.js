36c6902391ff9485d19dbf6a66560a29c140df13
SuzBarnes <SuzBarnes@github.com>
2022-07-13 22:58:26 +0100

    managed to get the console.log() working after restructuring

--- src/pet.js
+++ src/pet.js
@@ -4,42 +4,47 @@ const STARTATZERO = 0;
 const STARTFITNESS = 10;
 const YEARSAGED = 1;
 
-function Pet(name) {
-    this.name = name;
-    this.age = STARTATZERO,
-    this.hunger = STARTATZERO;
-    this.fitness = STARTFITNESS;
-    this.babies = [];
-
-    this.initialiseHouse;
+class Pet {
+    constructor(name){
+        this.name = name;
+        this.age = STARTATZERO,
+        this.hunger = STARTATZERO;
+        this.fitness = STARTFITNESS;
+        this.babies = [];
     
-    document.querySelector('#foodbutton').addEventListener('click', () => {
-        console.log(`hunger = ${this.hunger}`);
-        this.feedPet;
-        console.log(`hunger = ${this.hunger}`);
-    });
-
-    document.querySelector('#walkbutton').addEventListener('click', () => {
-        this.walk;
-        console.log(`fitness = ${this.fitness}`);
-    });
-
-    document.querySelector('#growupbutton').addEventListener('click', () =>
-    {
-        this.growUp;
-        console.log(`${this.age} years old, hunger = ${this.hunger}, fitness = ${this.fitness}`);
-    });
+        this.initialiseHouse();
+        
+        document.querySelector('#foodbutton').addEventListener('click', () => {
+            this.feedPet();
+            console.log(`${this.age} years old, hunger = ${this.hunger}, fitness = ${this.fitness}`);
+            
+        });
+    
+        document.querySelector('#walkbutton').addEventListener('click', () => {
+            this.walk();
+            console.log(`${this.age} years old, hunger = ${this.hunger}, fitness = ${this.fitness}`);
+        });
+    
+        document.querySelector('#growupbutton').addEventListener('click', () =>
+        {
+            this.growUp();
+            console.log(`${this.age} years old, hunger = ${this.hunger}, fitness = ${this.fitness}`);
+        });
+        
 
     /*document.querySelector('#checkupbutton').addEventListener('click', () => {
-        this.checkUp;
+        this.checkUp();
+        console.log(`${this.age} years old, hunger = ${this.hunger}, fitness = ${this.fitness}`);
     })*/
+    };
+    
 
-Pet.prototype = {
-    get isAlive() {
+
+    getisAlive() {
         return this.age <30 && this.hunger <10 && this.fitness >0;
     }
-};
-Pet.prototype.initialiseHouse = function () { /* for the room image to be placed as background */
+
+initialiseHouse() { /* for the room image to be placed as background */
     const backgrounds =[
         '../virtual-pet/images/room.png'
     ];
@@ -49,18 +54,18 @@ Pet.prototype.initialiseHouse = function () { /* for the room image to be placed
 
 };
 
-Pet.prototype.renderPet = function () { /*for the pet image to be put into the house*/
+renderPet() { /*for the pet image to be put into the house*/
     const pet = this.pet;
     const petElement = document.querySelectory('#pet');
 };
 
-Pet.prototype.renderFoodBowl = function () { /* empty food bowl image that stays there until filled for a few seconds */
+renderFoodBowl() { /* empty food bowl image that stays there until filled for a few seconds */
     const foodbowl = this.foodbowl;
     const foodbowlElement = document.querySelector('#foodbowl');
 }
 
-Pet.prototype.growUp = function () {
-    if (!this.isAlive){
+growUp() {
+    if (!this.getisAlive()){
         throw new Error ('Your pet is no longer alive :(');
     };
 this.age += YEARSAGED;
@@ -68,8 +73,8 @@ this.fitness -=3;
 this.hunger +=5;
 };
 
- Pet.prototype.checkUp = function() {
-    if (!this.isAlive){
+checkUp() {
+    if (!this.getisAlive()){
         throw new Error ('Your pet is no longer alive :(');
     };
         if (this.fitness<=3 && this.hunger<5){
@@ -82,8 +87,8 @@ this.hunger +=5;
         };
     };
   
-  Pet.prototype.walk = function() {
-    if (!this.isAlive){
+  walk() {
+    if (!this.getisAlive()){
         throw new Error ('Your pet is no longer alive :(');
     };
       if ((this.fitness + 4)<=MAXIMUM_FITNESS){
@@ -93,8 +98,8 @@ this.hunger +=5;
       }
   };
 
-  Pet.prototype.feedPet = function () {
-    if (!this.isAlive){
+ feedPet() {
+    if (!this.getisAlive()){
         throw new Error ('Your pet is no longer alive :(');
     };
       if((this.hunger-3)>=MINIMUM_HUNGER){
